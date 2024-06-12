62312d34eef78667a612ece78134b1fc41d6a315
electrolunch <e.lunch@yahoo.com>
2024-06-12 18:22:47 +0300

    1

--- content_poster.py
+++ content_poster.py
@@ -1,3 +1,4 @@
+from enum import IntEnum
 import os
 import time
 import vk_api
@@ -27,22 +28,94 @@ def captcha_handler(captcha):
     # Пробуем снова отправить запрос с капчей
     return captcha.try_again(key)
 
+class Permissions1(IntEnum):
+    """
+    Перечисление прав пользователя.
+    Список прав получается побитовым сложением (x | y) каждого права.
+    Подробнее в документации VK API: https://vk.com/dev/permissions
+    """
+
+    #: Пользователь разрешил отправлять ему уведомления
+    #: (для flash/iframe-приложений).
+    #: Не работает с этой библиотекой.
+    # NOTIFY = 1
+
+    #: Доступ к друзьям.
+    # FRIEND = 2
+
+    #: Доступ к фотографиям.
+    # PHOTOS = 2**2
+
+    #: Доступ к аудиозаписям.
+    #: При отсутствии доступа к закрытому API аудиозаписей это право позволяет
+    #: только загрузку аудио.
+    # AUDIO = 2**3
+
+    #: Доступ к видеозаписям.
+    # VIDEO = 2**4
+
+    #: Доступ к историям.
+    # STORIES = 2**6
+
+    #: Доступ к wiki-страницам.
+    # PAGES = 2**7
+
+    #: Добавление ссылки на приложение в меню слева.
+    # ADD_LINK = 2**8
+
+    #: Доступ к статусу пользователя.
+    # STATUS = 2**10
+
+    #: Доступ к заметкам пользователя.
+    # NOTES = 2**11
+
+    #: Доступ к расширенным методам работы с сообщениями.
+    # MESSAGES = 2**12
+
+    #: Доступ к обычным и расширенным методам работы со стеной.
+    WALL = 2**13
+
+    #: Доступ к расширенным методам работы с рекламным API.
+    # ADS = 2**15
+
+    #: Доступ к API в любое время. Рекомендуется при работе с этой библиотекой.
+    OFFLINE = 2**16
+
+    #: Доступ к документам.
+    DOCS = 2**17
+
+    #: Доступ к группам пользователя.
+    # GROUPS = 2**18
+
+    #: Доступ к оповещениям об ответах пользователю.
+    # NOTIFICATIONS = 2**19
+
+    #: Доступ к статистике групп и приложений пользователя, администратором которых он является.
+    # STATS = 2**20
+
+    #: Доступ к email пользователя.
+    # EMAIL = 2**22
+
+    #: Доступ к товарам.
+    # MARKET = 2**27
+
 class VkPoster(ContentPoster):
     def __init__(self):
-        self.login="+00000"
+        self.login="+79006398664"
         self.group_id = -129592796
-        self.password=")))))"
+        self.password="tgb_6!!jHG#6666"
         self.token="vk1.eeee"
         self.vk_session = vk_api.VkApi(
-            # login=self.login,
-            # password=self.password,
-            # auth_handler=self.auth_handler,
-            token="vk1.a.G-Te88L6KyW59H6Ev41iWdOp08j82djZS7McoRlvQJ7ZTbuzpYaCWJ4Z9u28I7HLn3PVnzAtiFSx55qVZPFh40ZWyEogoI-hsPUlJauGkTq_4P8OeI5l0t4GATcIvjXIcEkQ-01BVkDEYe9Wb6rnahxPV1Mlnn__HY8XNZStPSGuBwzCc6ziKu1y4D3NJyOiaRJckEqc9xCeOsNr7SU-WQ",
-            # captcha_handler=captcha_handler,# функция для обработки капчи
+            login=self.login,
+            password=self.password,
+            auth_handler=self.auth_handler,
+            # token="vk1.a.G-Te88L6KyW59H6Ev41iWdOp08j82djZS7McoRlvQJ7ZTbuzpYaCWJ4Z9u28I7HLn3PVnzAtiFSx55qVZPFh40ZWyEogoI-hsPUlJauGkTq_4P8OeI5l0t4GATcIvjXIcEkQ-01BVkDEYe9Wb6rnahxPV1Mlnn__HY8XNZStPSGuBwzCc6ziKu1y4D3NJyOiaRJckEqc9xCeOsNr7SU-WQ",
+            captcha_handler=captcha_handler,# функция для обработки капчи
             # app_id=7614654  
-            # app_id=3698024
+            # app_id=1,
+            scope=sum(Permissions1)
             )
-        # self.vk_session.auth()
+        self.vk_session.auth()
         self.vk = self.vk_session.get_api()
         self.upload = vk_api.VkUpload(self.vk_session)
 
